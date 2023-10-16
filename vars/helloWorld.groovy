9f991814c3a9cd3ee32751d231af7f6656d66689
nayab786910 <73090012+nayab786910@users.noreply.github.com>
2023-10-16 19:33:46 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
 def call(){
-   sh "git branch: 'main', credentialsId: 'git-hub', url: 'https://github.com/nayab786910/myspring-boot.git'"
+   git branch: 'main', credentialsId: 'git-hub', url: 'https://github.com/nayab786910/myspring-boot.git'
 }
