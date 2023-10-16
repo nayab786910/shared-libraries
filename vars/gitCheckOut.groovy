a8069b0ca107e161d0603cc335cfaab0923ba950
nayab786910 <73090012+nayab786910@users.noreply.github.com>
2023-10-16 19:10:02 +0530

    Update gitCheckOut.groovy

--- vars/gitCheckOut.groovy
+++ vars/gitCheckOut.groovy
@@ -1,3 +1,3 @@
 def call() {
- sh "git credentialsId: 'git-hub', url: 'https://github.com/nayab786910/myspring-boot.git'"
+ sh "checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'git-hub', url: 'https://github.com/nayab786910/shared-libraries.git']])"
 }
