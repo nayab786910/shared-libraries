45bc3125f6935a5f03334becbbf74729b001cbb8
nayab786910 <73090012+nayab786910@users.noreply.github.com>
2023-10-16 19:30:32 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
 def call(){
-   sh "git clone -b main https://github.com/nayab786910/myspring-boot.git"
+   sh "checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'git-hub', url: 'https://github.com/nayab786910/myspring-boot.git']])"
 }
